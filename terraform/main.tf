3cd0f60da0a0d6fced8d02bf1c0baded4d4b340a
Hari230798 <91759430+Hari230798@users.noreply.github.com>
2026-08-14 17:12:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,4 +39,52 @@ resource "aws_security_group" "k8s_sg" {
     from_port   = 10250
     to_port     = 10250
     protocol    = "tcp"
-    
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "NodePort"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = true
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "master" {
+
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.k8s_sg.id]
+
+  tags = {
+    Name = "k8s-master"
+  }
+}
+
+resource "aws_instance" "worker" {
+
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.k8s_sg.id]
+
+  tags = {
+    Name = "k8s-worker"
+  }
+}
