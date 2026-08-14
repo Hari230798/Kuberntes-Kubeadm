045111971d895c657dfee3b1b4c207b7baea1d91
Hari230798 <91759430+Hari230798@users.noreply.github.com>
2026-08-14 16:56:55 +0530

    Modify Terraform variables for AWS configuration
    Updated AWS region and added descriptions for ami_id and key_name variables. Introduced instance_type variable.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,14 @@
-variable "aws_region" {default="ap-south-1"}
-variable "ami_id" {}
-variable "key_name" {}
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "Ubuntu AMI"
+}
+
+variable "key_name" {
+  description = "AWS Key Pair Name"
+}
+
+variable "instance_type" {
+  default
