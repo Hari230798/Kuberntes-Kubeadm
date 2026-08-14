cd0d3d74f2f96d6f09280234b2fdec63253636dc
Hari230798 <91759430+Hari230798@users.noreply.github.com>
2026-08-14 16:56:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,42 @@
-provider "aws" { region = var.aws_region }
-resource "aws_security_group" "k8s_sg" { name="k8s-sg"
- ingress {from_port=22 to_port=22 protocol="tcp" cidr_blocks=["0.0.0.0/0"]}
- ingress {from_port=6443 to_port=6443 protocol="tcp" cidr_blocks=["0.0.0.0/0"]}
- egress {from_port=0 to_port=0 protocol="-1" cidr_blocks=["0.0.0.0/0"]}}
-resource "aws_instance" "master" { ami=var.ami_id instance_type="t3.medium" key_name=var.key_name vpc_security_group_ids=[aws_security_group.k8s_sg.id] tags={Name="k8s-master"}}
-resource "aws_instance" "worker" { ami=var.ami_id instance_type="t3.medium" key_name=var.key_name vpc_security_group_ids=[aws_security_group.k8s_sg.id] tags={Name="k8s-worker"}}
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "k8s_sg" {
+
+  name        = "k8s-security-group"
+  description = "Security Group for Kubernetes"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kubernetes API Server"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kubelet"
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    
